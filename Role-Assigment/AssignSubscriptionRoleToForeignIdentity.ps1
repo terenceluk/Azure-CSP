e37a8de3769a44bffba3e2a4792fa7082199ba44
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2022-05-17 11:01:59 -0400

    Update AssignSubscriptionRoleToForeignIdentity.ps1

--- Role-Assigment/AssignSubscriptionRoleToForeignIdentity.ps1
+++ Role-Assigment/AssignSubscriptionRoleToForeignIdentity.ps1
@@ -1,3 +1,18 @@
+<#
+The following PowerShell script will perform the following:
+
+1. Use the Connect-AzAccount to connect to Azure
+2. Retrieve the list of subscriptions and prompt the user to select one that already has the Foreign Principal assigned with a role
+3. Retrieve the list of Foreign Principals assigned to the subscription and prompt the user to select the one that will be used to assign a role for another subscription
+4. Retrieve the list of subscriptions again and prompt the user to select one that we are to assign the Foreign Principal assigned with a role
+5. Prompt the user to choose whether to assign the Foreign Principal the Owner or Reader role
+6. If Reader role is selected, prompt user if they want to also add the Support Request Contributor as well (tickets cannot be opened if the CSP Foreign Principal has Reader role but not Support Request Contributor role)
+7. Prompt the user to confirm the assignment with Y or N
+8. Output the result
+
+Please see my blog post here for more information: http://terenceluk.blogspot.com/2021/09/granting-csp-foreign-principal-reader.html
+#>
+
 # Connect to Azure tenant
 Connect-AzAccount
 
